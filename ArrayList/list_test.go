d52ba183569b586bba9df668e11357144501be1e
CXN <3203238028@qq.com>
2023-02-19 08:59:40 +0800

    DFS

--- ArrayList/list_test.go
+++ ArrayList/list_test.go
@@ -40,12 +40,40 @@ func leftBound(nums []int, target int) int {
 	return l + 1
 }
 
-//func TestCon(t *testing.T) {
-//	var nums []int = []int{1, 2, 4, 5, 6}
-//	c := Constructor2(nums)
-//	res := c.PickIndex()
-//	log.Print(res)
-//	l := len(nums) - 1
-//	r := rand.Intn(l) + 1
-//	log.Print(r)
-//}
+//	func TestCon(t *testing.T) {
+//		var nums []int = []int{1, 2, 4, 5, 6}
+//		c := Constructor2(nums)
+//		res := c.PickIndex()
+//		log.Print(res)
+//		l := len(nums) - 1
+//		r := rand.Intn(l) + 1
+//		log.Print(r)
+//	}
+func maxSlidingWindow(nums []int, k int) (res []int) {
+	left, right := 0, 0
+	mk := nums[0]
+	res = make([]int, 0, len(nums)-k+1)
+	var mmax func(int, int) int
+	mmax = func(r int, y int) int {
+		if r > y {
+			return r
+		}
+		return y
+	}
+	for right < len(nums) {
+		if k == 1 {
+			mk = nums[left]
+		}
+		mk = mmax(nums[right], mk)
+		right++
+		// 增大窗口
+		if right >= k {
+			res = append(res, mk)
+		}
+
+		for (right - left) >= k {
+			left++
+		}
+	}
+	return
+}
--- DFS/DFS_test.go
+++ DFS/DFS_test.go
@@ -1,7 +1,11 @@
 package DFS
 
 import (
+	"container/heap"
 	"github.com/stretchr/testify/assert"
+	"log"
+	"sort"
+	"strings"
 	"testing"
 )
 
@@ -110,4 +114,140 @@ func numIslands(grid [][]byte) (res int) {
 		}
 	}
 	return
+}
+
+func TestFinfsameFS(t *testing.T) {
+	cs := []string{"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"}
+	res := findDuplicate(cs)
+	log.Print(res)
+
+}
+
+// 重复文件
+func findDuplicate(paths []string) (res [][]string) {
+	record := make(map[string][]string, len(paths))
+	for _, v := range paths {
+		splis := strings.Split(v, " ")
+		dir := splis[0]
+		for _, v2 := range splis[1:] {
+			indexPosition := strings.IndexByte(v2, '(')
+			content := v2[indexPosition : len(v2)-1]
+			record[content] = append(record[content], dir+"/"+v2[:indexPosition])
+		}
+	}
+	for _, dirF := range record {
+		if len(dirF) >= 2 {
+			res = append(res, dirF)
+		}
+	}
+	return
+}
+
+func TestMaxSlidingWindow(t *testing.T) {
+
+	tastCase := []struct {
+		name  string
+		input []int
+		out   []int
+		big   int
+	}{
+		{
+			name:  "1",
+			input: []int{1, 3, -1, -3, 5, 3, 6, 7},
+			out:   []int{3, 3, 5, 5, 6, 7},
+			big:   3,
+		},
+		{
+			name:  "1",
+			input: []int{7, 2, 4},
+			out:   []int{7, 4},
+			big:   2,
+		},
+	}
+	for _, ts := range tastCase {
+		t.Run(ts.name, func(t *testing.T) {
+			res := maxSlidingWindowV1(ts.input, ts.big)
+			assert.Equal(t, ts.out, res)
+		})
+	}
+}
+
+// 滑动窗口
+func maxSlidingWindow(nums []int, k int) []int {
+	left, right := k-1, k-1
+	var mk int = maxList(nums[:k-1])
+	res := make([]int, 0, len(nums)-k+1)
+	res = append(res, mk)
+	for right < len(nums)-1 {
+
+		right++
+		mk = max(nums[right], mk)
+		// 增大窗口
+		if right-left == 1 {
+			res = append(res, mk)
+			left++
+		}
+	}
+	return res
+}
+func max(r int, y int) int {
+	if r > y {
+		return r
+	}
+	return y
+}
+func maxList(n []int) (res int) {
+	res = n[0]
+	for i := 0; i < len(n); i++ {
+		res = max(res, n[i])
+	}
+	return res
+}
+
+var a []int
+
+type hp struct{ sort.IntSlice }
+
+func (h hp) Less(i, j int) bool  { return a[h.IntSlice[i]] > a[h.IntSlice[j]] }
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
+func maxSlidingWindowV1(nums []int, k int) []int {
+	a = nums
+	q := &hp{make([]int, k)}
+	for i := 0; i < k; i++ {
+		q.IntSlice[i] = i
+	}
+	heap.Init(q)
+
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q.IntSlice[0]]
+	for i := k; i < n; i++ {
+		heap.Push(q, i)
+		for q.IntSlice[0] <= i-k {
+			heap.Pop(q)
+		}
+		ans = append(ans, nums[q.IntSlice[0]])
+	}
+	return ans
+}
+func pivotIndex(nums []int) int {
+	left, right := 0, len(nums)-1
+	leftNum, rightNum := 0, 0
+	for right < left-1 {
+		if leftNum <= rightNum {
+			leftNum = nums[left] + leftNum
+			left++
+		}
+		if leftNum <= rightNum {
+			rightNum = nums[right] + rightNum
+			right++
+		}
+	}
+	return left
 }
